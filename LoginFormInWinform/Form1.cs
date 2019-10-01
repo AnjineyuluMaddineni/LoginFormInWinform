a53329d2f3b138d396bb053626eacd91b86308da
lenovo <lenovo@DESKTOP-UP83FQL>
2019-10-01 18:42:25 +0530

    01 commit

--- LoginFormInWinform/Form1.cs
+++ LoginFormInWinform/Form1.cs
@@ -16,5 +16,23 @@ namespace LoginFormInWinform
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user, password;
+            user = textBox1.Text;
+            password = textBox2.Text;
+            if (user=="Maddineni"&& password=="sitecore")
+            {
+                //MessageBox.Show("successfully completed ");
+                label3.Text = ("Login successfully completed ");
+            }
+            else
+            {
+                //MessageBox.Show("failed please Entered correct password");
+                label3.Text = ("failed please Entered correct password");
+            }
+            
+        }
     }
 }
